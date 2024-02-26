e01f49120212b586d417e5ebf19c00f4e01feca4
IDimasaI <137656810+IDimasaI@users.noreply.github.com>
2024-02-26 17:04:39 +0300

    dasd

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React,{lazy} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Constellation_system = lazy(() => import('./router/App').then(module => ({default: module.Constellation_system})));
+const Regions = lazy(() => import('./router/App').then(module => ({default: module.Region_all})));
+if(document.getElementById('Constellation&System') as HTMLElement){
+  const root = ReactDOM.createRoot(document.getElementById('Constellation&System') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <Constellation_system />
+    </React.StrictMode>
+  );
+}
+if(document.getElementById('Regions') as HTMLElement){
+  const root = ReactDOM.createRoot(document.getElementById('Regions') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <Regions />
+    </React.StrictMode>
+  );
+}
